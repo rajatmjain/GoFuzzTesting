1d73e678555b66d6f16c7ffcaa987486b4f07f47
rjain032 <rajat.jain@email.ucr.edu>
2022-09-27 13:43:33 -0700

    Final

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"unicode/utf8"
 )
 
-func main(){
-	input := "The quick brown fox jumped over the lazy dog"
-    rev := Reverse(input)
-    doubleRev := Reverse(rev)
+func main() {
+    input := "The quick brown fox jumped over the lazy dog"
+    rev, revErr := Reverse(input)
+    doubleRev, doubleRevErr := Reverse(rev)
     fmt.Printf("original: %q\n", input)
-    fmt.Printf("reversed: %q\n", rev)
-    fmt.Printf("reversed again: %q\n", doubleRev)
+    fmt.Printf("reversed: %q, err: %v\n", rev, revErr)
+    fmt.Printf("reversed again: %q, err: %v\n", doubleRev, doubleRevErr)
 }
 
 // Byte approach
@@ -23,14 +25,13 @@ func main(){
 // }
 
 // Rune approach
-func Reverse(s string) string {
-    fmt.Printf("input: %q\n", s)
+func Reverse(s string) (string,error) {
+    if !utf8.ValidString(s) {
+        return s, errors.New("input is not valid UTF-8")
+    }
     r := []rune(s)
-    fmt.Printf("runes: %q\n", r)
     for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
         r[i], r[j] = r[j], r[i]
     }
-    return string(r)
+    return string(r),nil
 }
-
-// 
