315a0e6f8e82448ce386a3ef71a9237162406286
rjain032 <rajat.jain@email.ucr.edu>
2022-11-08 01:16:17 -0800

    Ints and Floats Schema and CustonFuzzFuncs

--- main.go
+++ main.go
@@ -7,16 +7,42 @@ import (
 )
 
 func main() {
-    var A string
-	var B int64
+	// String Fuzzer
+	var A string
 	unicodeRange := fuzz.UnicodeRange{First: 'A', Last: 'z',MaxLength: 25}
-	f := fuzz.New().Funcs(unicodeRange.CustomStringFuzzFunc())
-	f.Fuzz(&A) 
-    fmt.Println(A)
+	stringFuzzer := fuzz.New().Funcs(unicodeRange.CustomStringFuzzFunc())
+	stringFuzzer.Fuzz(&A)
+    fmt.Println("Random string: ",A)
+
+	// Integer64 Fuzzer
+	var B int64
 	int64Schema := fuzz.Int64Schema{Minimum: 0,Maximum: 10000}
-	f1 := fuzz.New().Funcs(int64Schema.CustomInt64FuzzFunc())
-	f1.Fuzz(&B)
-	fmt.Println(B)
+	int64Fuzzer := fuzz.New().Funcs(int64Schema.CustomInt64FuzzFunc())
+	int64Fuzzer.Fuzz(&B)
+	fmt.Println("Random integer64: ",B)
+
+	// Integer32 Fuzzer
+	var C int32
+	int32Schema := fuzz.Int32Schema{Minimum: 0,Maximum: 10000}
+	int32Fuzzer := fuzz.New().Funcs(int32Schema.CustomInt32FuzzFunc())
+	int32Fuzzer.Fuzz(&C)
+	fmt.Println("Random integer32: ",C)
+
+	// Float64 Fuzzer
+	var D float64
+	float64Schema := fuzz.Float64Schema{Minimum: 0,Maximum: 10000,Precision: 0}
+	float64Fuzzer := fuzz.New().Funcs(float64Schema.CustomFloat64FuzzFunc())
+	float64Fuzzer.Fuzz(&D)
+	fmt.Println("Random float64: ",D)
+
+	// Float32 Fuzzer
+	var E float32
+	float32Schema := fuzz.Float32Schema{Minimum: 0,Maximum: 10000,Precision: 0}
+	float32Fuzzer := fuzz.New().Funcs(float32Schema.CustomFloat32FuzzFunc())
+	float32Fuzzer.Fuzz(&E)
+	fmt.Println("Random float32: ",E)
+	
+
 } 
 
 
