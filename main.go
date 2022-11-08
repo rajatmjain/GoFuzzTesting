7b3af2bb139cf98f587dbb182a77796559001c4e
rjain032 <rajat.jain@email.ucr.edu>
2022-11-07 18:57:33 -0800

    Int64 Schema

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
+import (
+	"fmt"
+
+	fuzz "github.com/google/gofuzz"
+)
+
 func main() {
-    
+    var A string
+	var B int64
+	unicodeRange := fuzz.UnicodeRange{First: 'A', Last: 'z',MaxLength: 25}
+	f := fuzz.New().Funcs(unicodeRange.CustomStringFuzzFunc())
+	f.Fuzz(&A) 
+    fmt.Println(A)
+	int64Schema := fuzz.Int64Schema{Minimum: 0,Maximum: 10000}
+	f1 := fuzz.New().Funcs(int64Schema.CustomInt64FuzzFunc())
+	f1.Fuzz(&B)
+	fmt.Println(B)
 } 
 
-func IsPositiveAndEven(input int)(bool,bool){
-    if((input>0) && (input%2==0)){
-        return true,true
-    }else if((input<0) && (input%2==0)){
-        return false,true
-    }else if((input>0) && (input%2!=0)){
-        return true,false
-    }else{
-        return false,false
-    } 
-}
 
 
