8b1f8b1a37300548f44da340676ddb287d933947
rjain032 <rajat.jain@email.ucr.edu>
2022-11-07 18:58:13 -0800

    Int64 Schema

--- main_test.go
+++ main_test.go
@@ -1,25 +1,3 @@
 package main
 
-import (
-	"testing"
 
-	schemaBasedInputGenerator "github.com/rajatjain007/GoFuzzTesting/helper"
-)
-
-// func FuzzIsPositiveAndEven(f *testing.F) {
-//     f.Fuzz(func(t *testing.T, input int){
-//         isPositive,isEven := IsPositiveAndEven(input)
-//         if(isPositive!=true || isEven!=true){
-//             t.Errorf("Error!")
-//         }
-
-//     })
-// }
-
-func TestIsPositiveAndEven(t *testing.T) {
-    value := schemaBasedInputGenerator.ExampleCustomInt(schemaBasedInputGenerator.CustomInt{1,10000})
-    isPositive,isEven := IsPositiveAndEven(value)
-        if (isPositive != true && isEven != true) {
-                t.Errorf("Error!")
-        }
-}
