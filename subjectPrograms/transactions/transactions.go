e939c2c1404200d1751c04726d5554c8e90584b3
rjain032 <rajat.jain@email.ucr.edu>
2023-01-05 12:49:44 -0800

    GoFuzz

--- subjectPrograms/transactions/transactions.go
+++ subjectPrograms/transactions/transactions.go
@@ -7,6 +7,7 @@ import (
 
 func Deposit(username string, amount int64) error{
 	fmt.Println("Amount to be deposited:",amount)
+	// Assertion
 	if(amount>0 && amount<=10000){
 		fmt.Println(username,"\nAmount Deposited:",amount)
 	}else{
@@ -16,3 +17,12 @@ func Deposit(username string, amount int64) error{
 	
 	return nil
 }
+
+
+// task 1. coverage over time.
+// task 2: error/assertion injection, and report activated assertions over time for both gofuzz and "myfuzz"
+// testing goal: generate inputs to exercise as many branches or assertions as possible.
+// if (x is out of the range) assert("invalid amount of money")
+// each assertion represents one posible error
+// cumulative coverage over time
+// cumulative # of assertions over time
